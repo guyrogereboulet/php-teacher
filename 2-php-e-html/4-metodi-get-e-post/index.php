14e1860e879b09d3e1cccc7ffd86cfb4a3096a35
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-18 11:32:54 +0100

    Metodi Get e Post

---
+++ 2-php-e-html/4-metodi-get-e-post/index.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les bases de HTML/PHP</title>
+    <link rel="stylesheet" href="main.css"> 
+</head>
+<body>
+    <h1>I form</h1>
+    <!-- Ci sono 2 metodi per inviare delle informazioni al server,
+    metodo POST o GET -->
+    <form action="#" method = "POST">
+        <fieldset>
+            <legend>Information</legend>
+            <label for="nom">Nom :</label>
+            <input type="text" name="nom" id="nom" required>
+            <label for="nom">Age :</label>
+            <input type="number" name="age" id="age" required>
+            <input type="submit" value="envoyer">
+        </fieldset>
+    </form>
+
+    <?php 
+    //Con la funzione isset controllo che l'informazione non sia vuota
+    if(isset($_POST["nom"])) {
+    
+        echo "<div class ='backgroundColorGreen'>";
+            echo "Le nom est :" .$_POST["nom"]. "<br>";
+
+            if(isset($_POST["age"])) {
+
+            echo " L'age est :" .$_POST["age"];
+        }
+
+       echo "</div>";
+
+    }
+    
+    ?>
+
+</body>
+</html>
