f31f95e1abacd72512e9213d022d51da72086e67
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-19 14:19:57 +0100

    Tabellina del 4

---
+++ 3-esercizi/sito/commons/menu.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>      
+        <li><a href="index.php">Home</a></li>
+        <li><a href="exo1.php">Exo1</a></li>
+    </ul>
+</nav>
---
+++ 3-esercizi/sito/exo1.php
@@ -0,0 +1,41 @@
+<?php
+include("commons/head.php");
+include("commons/menu.php");
+?>
+<h1>Affichage des tables de multiplication</h1>
+
+<h2>Table de 4</h2>
+
+<?php 
+$quattre = 4;
+
+
+multiplication(0, $quattre);
+multiplication(1, $quattre);
+multiplication(2, $quattre);
+multiplication(3, $quattre);
+multiplication(4, $quattre);
+multiplication(5, $quattre);
+multiplication(6, $quattre);
+multiplication(7, $quattre);
+multiplication(8, $quattre);
+multiplication(9, $quattre);
+multiplication(10, $quattre);
+
+function multiplication($number1, $number2) {
+
+    $result = $number1 * $number2;
+
+    echo $number1."*".$number2." = ".$result."<br>";
+    
+
+}
+
+
+?>
+
+
+
+<?php 
+ include("../commons/footer.php");
+?>
