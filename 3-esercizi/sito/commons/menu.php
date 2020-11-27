50b246d4ebc0f4c5c33ecffc754e9a86caeb10a5
Guy Roger Eboulet <eboulet87@gmail.com>
2020-11-27 14:37:25 +0100

    Pyramide

--- 3-esercizi/sito/commons/menu.php
+++ 3-esercizi/sito/commons/menu.php
@@ -3,5 +3,7 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="exo1.php">Exo1</a></li>
         <li><a href="exo1-bis.php">Exo1-bis</a></li>
+        <li><a href="exo2.php">Exo2</a></li>
     </ul>
 </nav>
+
---
+++ 3-esercizi/sito/exo2.php
@@ -0,0 +1,52 @@
+<?php
+include("commons/head.php");
+include("commons/menu.php");
+?>
+<h1>Affichage d'une pyramide</h1>
+
+<form action="#" method="POST">
+
+<label for="hauteur">Hauteur de la pyramyde:</label>
+<input type="number" id="hauteur" name="hauteur">
+<button type="submit">Valider</button>
+</form>
+
+
+
+
+
+<?php 
+
+$hauteur = $_POST["hauteur"];
+
+
+
+    if (isset($hauteur) && $hauteur > 0) {
+
+        for ($i=0; $i < $hauteur; $i++) { 
+            $txt .= "**";
+            echo $txt."<br>";
+        }
+
+        for ($i=0; $i < $hauteur-1; $i++) { 
+            $txt = substr($txt,0,strlen($txt)-2);
+            echo $txt."<br>";
+        }
+
+
+    
+
+   
+    } else {
+        echo "<h2>Saisir une valeur svp</h2>";
+    }
+
+
+
+?>
+
+
+
+<?php 
+ include("commons/footer.php");
+?>
