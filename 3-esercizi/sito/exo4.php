38221bc115d655859f365ef3f712203a8ab72c7d
Guy Roger Eboulet <eboulet87@gmail.com>
2020-12-22 17:23:39 +0100

    les Session

--- 3-esercizi/sito/exo4.php
+++ 3-esercizi/sito/exo4.php
@@ -1,8 +1,23 @@
 <?php
-include("commons/head.php");
-include("commons/menu.php");
+    include("commons/head.php");
+    include("commons/menu.php");
+
+    // Mi permette di salvare un dato anche se mi sposto su un'altra pagina
+    session_start();
+
+    // se non esiste nella pagina la $_SESSION
+    if(!isset($_SESSION["chiffreAleatoire"])) {
+        //Con la funzione "rand" genero un nnumero aleatorio tra 2 estremi
+        $_SESSION["chiffreAleatoire"] = rand(1,10);
+    }
+
 ?>
 <h1>Trouver le nombre choisi par l'ordinateur</h1>
+
+<form action="#" method="POST">
+    <input type="hidden" name="reinit" value="yes">
+    <input type="submit" value="Reinitialiser">
+</form>
 
 <form action="#" method="POST">
     <label for="chiffre">Quel est le chiffre:</label>
@@ -18,9 +33,20 @@ include("commons/menu.php");
 
 <?php 
 
-$chiffreAleatoire = 5;
+
 
 $chiffre = $_POST["chiffre"];
+$reinit = $POST["reinit"];
+$chiffreAleatoire = $_SESSION["chiffreAleatoire"];
+
+
+if(isset($reinit) && $reinit === "yes") {
+    $chiffreAleatoire = rand(1,10);
+}
+// echo $chiffreAleatoire;
+
+
+
 
 if(isset($chiffre) && $chiffre > 0){
 
