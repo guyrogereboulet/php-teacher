67380435f93086b1f2d2d962ac9e9f601ce12663
Guy Roger Eboulet <eboulet87@gmail.com>
2020-12-23 14:45:16 +0100

    Exo5

--- 3-esercizi/sito/commons/menu.php
+++ 3-esercizi/sito/commons/menu.php
@@ -6,6 +6,7 @@
         <li><a href="exo2.php">Exo2</a></li>
         <li><a href="exo3.php">Exo3</a></li>
         <li><a href="exo4.php">Exo4</a></li>
+        <li><a href="exo5.php">Exo5</a></li>
     </ul>
 </nav>
 
