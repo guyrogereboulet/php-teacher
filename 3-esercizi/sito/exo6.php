2f7b68b03a34daa4cdd512f03efb1b353a83a880
Guy Roger Eboulet <eboulet87@gmail.com>
2020-12-28 20:48:43 +0100

    Commit

--- 3-esercizi/sito/exo6.php
+++ 3-esercizi/sito/exo6.php
@@ -5,13 +5,13 @@
 
 
 <h1>Selection du personnage </h1>
-<form action="#" method="GET">
+<form action="#" method="POST">
 
 <select name="personnage" id="personnage">
     <option value="homme">Homme</option>
     <option value="femme">Femme</option>
 </select>
-    <input type="submit" value="valider">
+    <input type="submit" value="valider" name="valider">
 </form>
 
 
@@ -29,41 +29,13 @@
 
 <?php 
 
-if(isset($_GET["nb"]) && $_GET["nb"] > 0) {
+if(isset($_POST["valider"])){
 
-    $nbNotes = $_GET["nb"];
+            if(isset($_POST["personnage"])){
 
-    echo "<form action=\"#\" method=\"POST\">";
-    echo "<fieldset>";
-    echo "<legend>Moyenne : </legend>";
-
-    for($i = 1; $i <= $nbNotes; $i++){
-
-        echo "<label for=\"nb\">Note" .$i. " : </label>";
-        echo "<input type=\"number\" name=\"note".$i."\" id=\"note" .$i."\" required><br>";
-
-    }
-    echo "<input type=\"submit\" value=\"Calculer\">";
-    echo "</fieldset>";
-    echo "</form>";
-
-    if(isset($_POST["note1"])){
-
-        $res = 0;
-        for($i = 1; $i <= $nbNotes; $i++){
-
-            $res =  $res + $_POST["note".$i];
+                echo "Ciao";
 
         }
-        echo "La moyenne est : ". $res / $nbNotes;
-
-    }
-
-   
-
-} else {
-
-     echo "<p>C'est ok</p>";
 
 }
 
