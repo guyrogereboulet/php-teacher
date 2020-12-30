d9eaa6cd6b8cc76e31bdcf65ded747302a78c5b4
Guy Roger Eboulet <eboulet87@gmail.com>
2020-12-30 16:45:23 +0100

    Commit

--- 3-esercizi/sito/exo7.php
+++ 3-esercizi/sito/exo7.php
@@ -9,6 +9,12 @@
 <h2>Etape 1</h2>
 <p>Réaliser un tableaux concernant les valeurs: </p>
 
+<h2>Etape 2</h2>
+<p>Réaliser une fonction qui vérifie si un tableau contient que des valeurs paires ou non </p>
+
+<h2>Etape 3</h2>
+<p>Affiche un message pour indiquer à l'utilisateur </p>
+
 
 
 <?php 
